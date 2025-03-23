5f32feb640b97aa94d27d2032132ccfac2734fc7
clema363 <clemallain363@gmail.com>
2025-03-23 16:56:11 +0100

    modifier et suprimer des notes

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,5 +1,9 @@
 import prisma from "@/lib/db/prisma";
-import { createNoteSchema, updateNoteSchema } from "@/lib/validation/note";
+import {
+  createNoteSchema,
+  deleteNoteSchema,
+  updateNoteSchema,
+} from "@/lib/validation/note";
 import { auth } from "@clerk/nextjs";
 
 export async function POST(req: Request) {
@@ -72,6 +76,42 @@ export async function PUT(req: Request) {
     });
 
     return Response.json({ updatedNote }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: Request) {
+  try {
+    const body = await req.json();
+
+    const parseResult = deleteNoteSchema.safeParse(body);
+
+    if (!parseResult.success) {
+      console.error(parseResult.error);
+      return Response.json({ error: "Invalid input" }, { status: 400 });
+    }
+
+    const { id } = parseResult.data;
+
+    const note = await prisma.note.findUnique({
+      where: { id },
+    });
+
+    if (!note) {
+      return Response.json({ error: "Note not found" }, { status: 404 });
+    }
+
+    const { userId } = auth();
+
+    if (!userId || userId !== note.userId) {
+      return Response.json({ error: "Unhauthorized" }, { status: 401 });
+    }
+
+    await prisma.note.delete({ where: { id } });
+
+    return Response.json({ message: "Note deleted" }, { status: 200 });
   } catch (error) {
     console.error(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
