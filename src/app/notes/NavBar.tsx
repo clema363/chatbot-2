07c7d9ed2668f878b8e4f5d2c0b9d9d76c2da0b7
clema363 <clemallain363@gmail.com>
2025-03-23 16:29:44 +0100

    creation des notes

---
+++ src/app/notes/NavBar.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddNoteDialog from "@/components/AddNoteDialog";
+
+export default function Navbar() {
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+
+  return (
+    <>
+      <div className="p-4 shadow">
+        <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
+          <Link href="/notes" className="flex items-center gap-1">
+            <Image src={logo} alt="MyChatbot Logo" width={40} height={40} />
+            <span className="font-bold">My Chatbot</span>
+          </Link>
+          <div className="itmes-center flex gap-2">
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
+            <Button onClick={() => setShowAddNoteDialog(true)}>
+              <Plus size={20} className="mr-2" />
+              Add Note
+            </Button>
+          </div>
+        </div>
+      </div>
+      <AddNoteDialog open={showAddNoteDialog} setOpen={setShowAddNoteDialog} />
+    </>
+  );
+}
--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const PrismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma };
+type PrismaClientSingletonType = ReturnType<typeof PrismaClientSingleton>;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingletonType | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? PrismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
