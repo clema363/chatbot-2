233eed58b53e8b93fe76a225d9e59a2c4db09b29
clema363 <clemallain363@gmail.com>
2025-03-24 17:06:59 +0100

    changement logo et favicon

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
+import logowhite from "@/assets/logo-white.png";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
@@ -21,7 +22,17 @@ export default function Navbar() {
       <div className="p-4 shadow">
         <div className="m-auto flex max-w-7xl flex-wrap items-center justify-between gap-3">
           <Link href="/notes" className="flex items-center gap-1">
-            <Image src={logo} alt="MyChatbot Logo" width={40} height={40} />
+            {theme === "dark" && (
+              <Image
+                src={logowhite}
+                alt="MyChatbot Logo"
+                width={40}
+                height={40}
+              />
+            )}
+            {theme !== "dark" && (
+              <Image src={logo} alt="MyChatbot Logo" width={40} height={40} />
+            )}
             <span className="font-bold">My Chatbot</span>
           </Link>
           <div className="itmes-center flex gap-2">
